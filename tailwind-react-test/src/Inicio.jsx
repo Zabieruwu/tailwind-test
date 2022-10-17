8f54c1362c71ae14627b0f63079cef3551c80bab
Zabieruwu <megajavier2000@gmail.com>
2022-10-17 19:17:33 -0300

    aplying tailwind into the className of react

---
+++ tailwind-react-test/src/Inicio.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Inicio() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+         Hello world!
+    </h1>
+  );
+}
+
+export default Inicio;
---
+++ tailwind-react-test/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/App.jsx",
+            "./src/index.css",
+            "./src/Inicio.jsx"
+    ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
